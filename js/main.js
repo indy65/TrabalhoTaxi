c17693079a62c0768170ef308ae3e3a9ab30cc3f
Your Name <you@example.com>
2021-09-30 02:36:07 +0100

    30-9 late night submit

--- js/main.js
+++ js/main.js
@@ -4,25 +4,26 @@ let input = document.getElementById("km")
 input.addEventListener("keypress", function (event) {
 
     if (event.key === 'Enter') {
-        // Cancel the default action, if needed
+        
         event.preventDefault();
-        // Trigger the button element with a click
+        
         document.getElementById("btn_click").click();
     }
 })
 
 
 function makeCall() {
-    window.location.href = 'tel:966151547'
+    window.location.href = 'tel:+351915354381'
 }
 
 function simulate() {
     let km = document.getElementById("km").value
     let total = document.getElementById("total")
+    let luggage=document.getElementById("luggage").value
     if (km >= 0) {
        
 
-        total.innerHTML = (3.9 + (km * 0.94)).toFixed(2).toString() + "€ a " + (3.9 + (km * 1.13)).toFixed(2).toString() + "€"
+        total.innerHTML = (3.9 + (km * 0.94)+(luggage*1.6)).toFixed(2).toString() + "€ a " + (3.9 + (km * 1.13)+(luggage*1.6)).toFixed(2).toString() + "€"
     }
     else{
         total.innerHTML="0 €"
