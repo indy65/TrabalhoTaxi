c27756caf23ec37579b68d648d11de0c5330e3b4
Your Name <you@example.com>
2021-10-01 01:56:07 +0100

    1/10

--- js/main.js
+++ js/main.js
@@ -19,11 +19,11 @@ function makeCall() {
 function simulate() {
     let km = document.getElementById("km").value
     let total = document.getElementById("total")
-    let luggage=document.getElementById("luggage").value
+  //  let luggage=document.getElementById("luggage").value
     if (km >= 0) {
        
 
-        total.innerHTML = (3.9 + (km * 0.94)+(luggage*1.6)).toFixed(2).toString() + "€ a " + (3.9 + (km * 1.13)+(luggage*1.6)).toFixed(2).toString() + "€"
+        total.innerHTML = (3.9 + (km * 0.94)).toFixed(2).toString() + "€ a " + (3.9 + (km * 1.13)).toFixed(2).toString() + "€"
     }
     else{
         total.innerHTML="0 €"
